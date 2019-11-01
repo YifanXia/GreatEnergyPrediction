ccefbe01a6eb1ae7f983da40a885fa1d932d4786
Yifan Xia <xiayifanisme@gmail.com>
2019-11-01 22:11:04 +0100

    added weather plotting

--- exploration_building.py
+++ exploration_building.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas.plotting import register_matplotlib_converters
 import numpy as np
 import os
 
@@ -7,14 +8,14 @@ def read_building_metadata(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 def plot_building_meter_reading(building_data: pd.DataFrame, site_id: int, metadata: pd.DataFrame) -> None:
-    for building_id in metadata[metadata.site_id == site_id].building_id.values:
+    for building_id, primary_use in metadata[metadata.site_id == site_id][['building_id', 'primary_use']].values:
         building_data_id = building_data[building_data.building_id == building_id]
         building_data_id.loc[:, 'log_meter_reading'] = np.log(building_data_id.meter_reading + 1)
         for meter_id in building_data_id.meter.unique():
             building_data_id_meter = building_data_id[building_data_id.meter == meter_id]
-            plt.figure(figsize=(10, 5))
-            plt.plot(building_data_id_meter.timestamp, building_data_id_meter.log_meter_reading)
-            plt.title('building: ' + str(building_id) + ', meter_id: ' + str(meter_id))
+            plt.figure(figsize=(14, 5))
+            plt.scatter(building_data_id_meter.timestamp, building_data_id_meter.log_meter_reading, marker='.', s=3, c='blue')
+            plt.title(f'building: {building_id} {primary_use}, meter_id: {meter_id}')
             if not os.path.exists('figs_site_' + str(site_id)):
                 os.mkdir('figs_site_' + str(site_id))
             if not os.path.exists('figs_site_' + str(site_id) + '/meter_' + str(meter_id)):
@@ -30,6 +31,7 @@ def plot_weather_data(weather_data: pd.DataFrame, site_id: int) -> pd.DataFrame:
         
 if __name__ == "__main__":
     
+    register_matplotlib_converters()
     data_path = "../data/train.csv"
     meta_data_path = "../data/building_metadata.csv"
     building_data = pd.read_csv(data_path, parse_dates=['timestamp'])
---
+++ exploration_weather.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from preprocessing import read_weather_data
+from features import add_feels_like
+
+def plot_one_site(weather_data: pd.DataFrame, site_id: int, weather: str) -> None:
+    data = weather_data[weather_data.site_id == site_id]
+    plt.figure(figsize=(14, 5))
+    plt.scatter(data['timestamp'], data[weather], marker='.', s=3, c='blue')
+    plt.title(f'site: {site_id}, weather: {weather}')
+    if not os.path.exists(f'figs_site_{site_id}'):
+        os.mkdir(f'figs_site_{site_id}')
+    if not os.path.exists(f'figs_site_{site_id}/weather'):
+        os.mkdir(f'figs_site_{site_id}/weather')
+    plt.savefig(f'figs_site_{site_id}/weather/{weather}.png', format='png')
+    plt.close()
+    
+def plot_weather_by_site(weather_data: pd.DataFrame) -> None:
+    for site_id in weather_data['site_id'].unique():
+        for weather in weather_data.columns:
+            plot_one_site(weather_data, site_id, weather)
+
+if __name__ == "__main__":
+    register_matplotlib_converters()
+    weather_data = read_weather_data('../data/weather_train.csv')
+    add_feels_like(weather_data)
+    plot_weather_by_site(weather_data)
