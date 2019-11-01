02259bd5b694a2c6ca62a317400a139a337ed572
Yifan Xia <xiayifanisme@gmail.com>
2019-11-01 15:52:23 +0100

    added new weather features

--- features.py
+++ features.py
@@ -34,12 +34,77 @@ def get_month_primary_use(data: pd.DataFrame) -> None:
 def transform_wind_direction(data: pd.DataFrame) -> None:
     data.loc[:, 'wind_direction_sin'] = pd.Series(np.sin(np.pi * data.wind_direction.values / 360))
 
+def add_wind_chill(data: pd.DataFrame) -> None:
+    def compute_wind_chill(row: pd.Series) -> float:
+        v = row['wind_speed'] * 3.6
+        T_a = row['air_temperature']
+        if T_a <= 10 and v >= 4.8:
+            return 13.12 + 0.6215 * T_a - 11.37 * v ** 0.16 + 0.3965 * T_a * v ** 0.16
+        return float('nan')
+    data.loc[:, 'wind_chill'] = data.apply(compute_wind_chill, axis=1)
+
+def add_relative_humidity(data: pd.DataFrame) -> None:
+    def compute_relative_humidity(row: pd.Series) -> float:
+        # Magnus formula
+        T_a = row['air_temperature']
+        T_dp = row['dew_temperature']
+        b = 18.678
+        c = 257.14 # Celsius
+        return 100 * np.exp(T_dp * b / (T_dp + c) - b * T_a / (c + T_a))
+    data.loc[:, 'relative_humidity'] = data.apply(compute_relative_humidity, axis=1)
+    
+def add_heat_index(data: pd.DataFrame) -> None:
+    def compute_heat_index(row: pd.Series) -> float:
+        T = row['air_temperature']
+        R = row['relative_humidity']
+        if R >= 40.0 and T >= 27.0:
+            c = [0] * 9
+            c[0] = -8.78469475556
+            c[1] = 1.61139411
+            c[2] = 2.33854883889
+            c[3] = -0.14611605
+            c[4] = -0.012308094
+            c[5] = -0.0164248277778
+            c[6] = 0.002211732
+            c[7] = 0.00072546
+            c[8] = -0.000003582
+        
+            vals = [1] * 9
+            vals[1] = T
+            vals[2] = R
+            vals[3] = T * R
+            vals[4] = T **2
+            vals[5] = R **2
+            vals[6] = T **2 * R
+            vals[7] = T * R **2
+            vals[8] = (T * R) **2
+        
+            return sum(a * b for a, b in zip(c, vals))
+        return float('nan')
+    data.loc[:, 'heat_index'] = data.apply(compute_heat_index, axis=1)
+
+def add_feels_like(data: pd.DataFrame, remove_others: bool = False) -> None:
+    add_relative_humidity(data)
+    add_heat_index(data)
+    add_wind_chill(data)
+    data.loc[:, 'feels_like'] = data['heat_index'].combine_first(data['wind_chill']).combine_first(data['air_temperature'])
+    if remove_others:
+        data.drop(['heat_index', 'wind_chill'], axis=1)
+
 def fill_na_in_weather_data(weather_data: pd.DataFrame) -> None:
     weather_data = weather_data.fillna(method='backfill').fillna(method='ffill')
 
 def add_yesterday_lag_features(weather_data: pd.DataFrame, window_size: int = 24) -> None:
     group_df = weather_data.groupby('site_id')
-    cols_mean = ['air_temperature', 'dew_temperature', 'precip_depth_1_hr', 'sea_level_pressure', 'wind_speed']
+    cols_mean = [
+        'air_temperature', 
+        'dew_temperature', 
+        'precip_depth_1_hr', 
+        'sea_level_pressure', 
+        'wind_speed', 
+        'relative_humidity', 
+        'feels_like'
+        ]
     cols_median = ['cloud_coverage']
     rolled_mean = group_df[cols_mean].rolling(window=window_size, min_periods=0, win_type='triang')
     rolled_median = group_df[cols_median].rolling(window=window_size, min_periods=0, win_type=None)
--- preprocessing.py
+++ preprocessing.py
@@ -4,14 +4,25 @@ from typing import Tuple
 import logging
 
 def read_building_metadata(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
+    return reduce_mem_usage(pd.read_csv(path))
+
+def read_building_data(path: str, remove_zeros: bool = False) -> pd.DataFrame:
+    data = pd.read_csv(path, parse_dates=['timestamp'])
+    if remove_zeros:
+        data = data.query('meter_reading > 0')
+    return reduce_mem_usage(data)
+
+def read_weather_data(path: str) -> pd.DataFrame:
+    data = pd.read_csv(path, parse_dates=['timestamp'])
+    return reduce_mem_usage(data)
 
 def log_transform_targets(data: pd.DataFrame) -> None:
     data.loc[:, 'log_meter_reading'] = np.log1p(data.loc[:, 'meter_reading'])
 
-def read_data(data_path: str, weather_path: str, meta_data: pd.DataFrame, nrows: int = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    building_data = pd.read_csv(data_path, parse_dates=['timestamp'], nrows=nrows)
-    weather_data = pd.read_csv(weather_path, parse_dates=['timestamp'])
+def read_data(data_path: str, weather_path: str, meta_data: pd.DataFrame,
+              remove_zeros: bool = False, nrows: int = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    building_data = read_building_data(data_path, remove_zeros)
+    weather_data = read_weather_data(weather_path)
     building_meta = building_data.merge(meta_data, on='building_id', how='left')
     #building_weather_meta = building_meta.merge(weather_data, on=['site_id', 'timestamp'], how='left')
     building_meta = reduce_mem_usage(building_meta)
